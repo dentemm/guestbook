ff7ab45198b065f4f4263ce790f962ac9c1d6847
Tim Claes <tim.claes@me.com>
2017-10-24 10:16:53 +0200

    webpak setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,20 +1,28 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
+module.exports = (env) => {
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                },
+                exclude: /node_modules/
+            }]
+        },
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+        }
     }
 };
